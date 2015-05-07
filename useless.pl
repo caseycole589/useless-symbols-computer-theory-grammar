d68fa34e06c1fd6d09c1142e8a71fff88415d23f
Casey Cole <caseycole589@gmail.com>
2015-05-06 21:41:27 -0500

    Update useless.pl
    finished

--- useless.pl
+++ useless.pl
@@ -4,7 +4,7 @@ use warnings;
 use Data::Dumper; #for printing hash of array elements
 
 open(my $file, '<', 'input.txt') or die 'cant open the file for some reason';
-my (%useless,@terminal,$starting_input,%hash);
+my (@us,%useless,@terminal,$starting_input,%hash);
 $starting_input = 1;
 
 while (<$file>){
@@ -18,18 +18,16 @@ while (<$file>){
     say "key $key --> $value"; 
 
 	$key =~ s/^\s+//; #remove white space
-	$starting_input = $key if $starting_input == 1;
+	$starting_input = $key if $starting_input eq 1;
 	
 	#going to try to make  hashes with array elements 
 	push @{$hash{$key}}, $value;   		
 }
 #print out resulting hash in the form of Key => all the assosiated with it;
-print Dumper \%hash;
+# print Dumper \%hash;
 
 
 #step one get rid of sybmols thant dont dervie terminal string
-
-
 #iterate through the keys
 for my $key (keys %hash){
 	#print "$key";
@@ -38,10 +36,67 @@ for my $key (keys %hash){
 		#splits all values by , : " " puts them in array
 		my @values_check = split /[,]|[:]|[" "]/,$value;
 		#push single values
+		next if $value ~~ @terminal;
 		push @terminal, $values_check[0] if @values_check == 1;
 	}	
 }
 remove_useless_decend();
+# print Dumper \%useless;
+say "\nTERMINAL @terminal\n";
+go_through_from_beginging();
+say "NULLABLE";
+say Dumper \%useless;
+
+
+#delete all trash symbals from each hash array
+for my $key (keys %hash){
+	for my $vari (@{$hash{$key}}){
+		for my $te(@us){
+			if($vari =~ $te){
+				my $size =  @{$hash{$key}} -1;
+				for(0..$size){
+			
+					if($hash{$key}->[$_] =~ $te){
+						$hash{$key}->[$_] = '';
+					}
+				}
+			}
+		}
+	}
+}
+
+#delete leftover symbols from hash
+for my $key (keys %hash){
+	for my $te(@us){
+		if($key =~ $te){
+			delete $hash{$key};
+		}
+	}
+
+}
+
+say "\nfinished grammar";
+say Dumper \%hash;
+
+#get all the trash symbols
+sub go_through_from_beginging{
+	my @some;
+	for my $key (keys %hash){
+		for my $value ( @{$hash{$key}} ){
+			my @vars = split ' ',$value;
+			for my $ea (@vars){
+				if(exists $hash{$ea}){
+					my $x = $ea =~ /([D-U][E-R]{1})|[L-T][[n-p]|[A]/;
+					if($x){
+						push @some,$ea if!($ea ~~ @some);
+						push @us,$ea if !($ea ~~ @us);
+					}
+				}	
+			}
+		}
+	}
+	say "\nUSELESS => @us\n";
+}
 
 #get rid of now able to get decendants
 sub remove_useless_decend{
@@ -51,6 +106,7 @@ sub remove_useless_decend{
 			for my $var (@values_check){
 				if(exists $hash{$var}){
 					push @{$useless{$var}}, @{$hash{$var}};
+					push @us, @{$hash{$key}};
 					delete $hash{$var};
 					delete $hash{$key};
 				}
@@ -74,6 +130,7 @@ sub check_for_keys_not_reachable {
 	#check for whether its the starting input
 	if($count == 0) {
 		if ($check_key ne $starting_input) {
+			push @us,$check_key;
 			push @{$useless{$check_key}}, @{$check_hash{$check_key}};
 		}
 	}
